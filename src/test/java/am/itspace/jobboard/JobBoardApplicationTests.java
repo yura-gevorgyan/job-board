4e0a4169d7736587fd087f021ecf144a5a4c5141
Yura Gevorgyan <yurgevorgyan02@gmail.com>
2024-03-03 15:34:08 +0400

    Initial commit

---
+++ src/test/java/am/itspace/jobboard/JobBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.jobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
