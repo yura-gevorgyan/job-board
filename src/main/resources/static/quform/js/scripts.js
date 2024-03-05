6b813d19a69c2d3f13dbbe6e9b1a26ec1caeab5f
Lyova Hakobyan <lyov123lyov@gmail.com>
2024-03-05 15:03:34 +0400

    Template Bug Fixing

--- src/main/resources/static/quform/js/scripts.js
+++ src/main/resources/static/quform/js/scripts.js
@@ -1,6 +1,28 @@
-'use strict';jQuery(function($){$('form.quform').Quform();if(window.tippy){$('.quform-tooltip').each(function(){tippy(this,{theme:'quform'});});}
-$('#subject').replaceSelectWithTextInput({onValue:'Other'});});(function($){$(window).on('load',function(){var images=['quform/images/close.png','quform/images/success.png','quform/images/error.png','quform/images/default-loading.gif'];if($('.quform-theme-light-light, .quform-theme-light-rounded').length){images=images.concat(['quform/themes/light/images/button-active-bg-rep.png','quform/themes/light/images/close.png','quform/themes/light/images/input-active-bg-rep.png']);}
-if($('.quform-theme-dark-dark, .quform-theme-dark-rounded').length){images=images.concat(['quform/themes/dark/images/button-active-bg-rep.png','quform/themes/dark/images/close.png','quform/themes/dark/images/input-active-bg-rep.png','quform/themes/dark/images/loading.gif']);}
-if($('.quform-theme-minimal-light').length){images=images.concat(['quform/themes/minimal/images/close-light.png']);}
-if($('.quform-theme-minimal-dark').length){images=images.concat(['quform/themes/minimal/images/close-dark.png','quform/themes/minimal/images/loading-dark.gif']);}
-$.preloadImages(images);});})(jQuery);
+'use strict';
+jQuery(function ($) {
+    $('form.quform').Quform();
+    if (window.tippy) {
+        $('.quform-tooltip').each(function () {
+            tippy(this, {theme: 'quform'});
+        });
+    }
+    $('#subject').replaceSelectWithTextInput({onValue: 'Other'});
+});
+(function ($) {
+    $(window).on('load', function () {
+        var images = ['/static/quform/images/close.png', '/static/quform/images/success.png', '/static/quform/images/error.png', '/static/quform/images/default-loading.gif'];
+        if ($('.quform-theme-light-light, .quform-theme-light-rounded').length) {
+            images = images.concat(['/static/quform/themes/light/images/button-active-bg-rep.png', '/static/quform/themes/light/images/close.png', '/static/quform/themes/light/images/input-active-bg-rep.png']);
+        }
+        if ($('.quform-theme-dark-dark, .quform-theme-dark-rounded').length) {
+            images = images.concat(['/static/quform/themes/dark/images/button-active-bg-rep.png', '/static/quform/themes/dark/images/close.png', '/static/quform/themes/dark/images/input-active-bg-rep.png', '/static/quform/themes/dark/images/loading.gif']);
+        }
+        if ($('.quform-theme-minimal-light').length) {
+            images = images.concat(['/static/quform/themes/minimal/images/close-light.png']);
+        }
+        if ($('.quform-theme-minimal-dark').length) {
+            images = images.concat(['/static/quform/themes/minimal/images/close-dark.png', '/static/quform/themes/minimal/images/loading-dark.gif']);
+        }
+        $.preloadImages(images);
+    });
+})(jQuery);
